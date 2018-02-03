1a327daad6b37d206165c3956d56272d37847934
bradcgarner <brad@bradgarner.com>
2018-02-03 11:33:13 -0500

    started c++

--- node-express-starter/auth/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const {router} = require('./router');
-
-const {basicStrategy, jwtStrategy} = require('./strategies');
-
-
-module.exports = {router, basicStrategy, jwtStrategy};
---
+++ testing/cpp-notes.cpp
@@ -0,0 +1,13 @@
+int main()
+{
+  return 0;
+}
+
+// DUH
+// case-sensitive
+// brackets must match
+
+// whitespace doesn't matter
+// ; to end lines - matters!
+// not all apps are main()
+// compiler errors and linker errors are different
